05a794d43afe08c1f6280684ac5be8edc5871156
etccoop <team@etccooperative.org>
2022-07-29 17:06:22 +0000

    add blockscout URL based on #310

--- gnosis/eth/clients/blockscout_client.py
+++ gnosis/eth/clients/blockscout_client.py
@@ -37,6 +37,7 @@ class BlockscoutClient:
         EthereumNetwork.REI_MAINNET: "https://scan.rei.network/",
         EthereumNetwork.REI_TESTNET: "https://scan-test.rei.network/",
         EthereumNetwork.ETC_MORDOR: "https://blockscout.com/etc/mordor/",
+        EthereumNetwork.ETHEREUM_CLASSIC_TESTNET_MORDOR: "https://blockscout.com/etc/mordor/",
         EthereumNetwork.METER: "https://scan.meter.io/",
         EthereumNetwork.METER_TESTNET: "https://scan-warringstakes.meter.io/",
         EthereumNetwork.GODWOKEN_TESTNET: "https://v1.betanet.gwscan.com/",
